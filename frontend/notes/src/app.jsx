4d564feaa218f762ddcb4959e8260ae429cb2c31
Aryan-git13 <rbaryantomar@gmail.com>
2026-03-13 16:08:06 +0530

    Update app.jsx

--- frontend/notes/src/app.jsx
+++ frontend/notes/src/app.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import SignUp from "./pages/Signup/Signup";
-import Login from "./pages/Login/Login";
+import Home from "./pages/home/home";
+import SignUp from "./pages/signup/signup";
+import Login from "./pages/login/login";
 
 const App = () => {
   return (
