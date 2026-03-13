0c335eaae94d8ec5c7bf6d7d07a3d5c292e23c17
Aryan-git13 <rbaryantomar@gmail.com>
2026-03-13 17:59:05 +0530

    Update axiosinstance.js

--- frontend/notes/src/utils/axiosinstance.js
+++ frontend/notes/src/utils/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://stingy-1.onrender.com/api/notes,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
